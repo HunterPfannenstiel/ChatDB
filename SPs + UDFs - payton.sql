fb9bb2a827086bc31935f6af252545a40522cc2b
Payton Pfannenstiel <111666963+Pfannen@users.noreply.github.com>
2023-04-17 13:39:57 -0500

    Adjusted FetchFollowers/Following to take in a user's handle

--- SPs + UDFs - payton.sql
+++ SPs + UDFs - payton.sql
@@ -1,43 +1,37 @@
---'FetchFollowers' means fetch the users that @userId follows.
-CREATE OR ALTER FUNCTION Chat.FetchFollowers (
-	@userId INT,
+--'FetchFollowing' means fetch the users that @userId follows.
+CREATE OR ALTER PROCEDURE Chat.FetchFollowing
+	@userHandle NVARCHAR(30),
 	@page INT
-)
-RETURNS TABLE
 AS
-RETURN (
-	SELECT U.[name] AS userName,
-		U.handle AS userHandle,
-		I.imageUrl AS userImage,
-		U.bio AS userBio
-	FROM Chat.[User] U 
-		INNER JOIN Chat.Follower F ON @userId = F.followerUserId
-			AND U.userId = F.followedUserId
-		INNER JOIN Chat.[Image] I ON U.imageId = I.imageId
-	ORDER BY F.followDate DESC
-	OFFSET (@page * 20) ROWS FETCH NEXT 20 ROWS ONLY
-)
+DECLARE @userId INT = (SELECT U.userId FROM Chat.[User] U WHERE U.handle = @userHandle);
+SELECT U.[name] AS userName,
+	U.handle AS userHandle,
+	I.imageUrl AS userImage,
+	U.bio AS userBio
+FROM Chat.[User] U 
+	INNER JOIN Chat.Follower F ON @userId = F.followerUserId
+		AND U.userId = F.followedUserId
+	INNER JOIN Chat.[Image] I ON U.imageId = I.imageId
+ORDER BY F.followDate DESC
+OFFSET (@page * 20) ROWS FETCH NEXT 20 ROWS ONLY
 GO
 
---'FetchFollowing' means fetch the users that follow @userId.
-CREATE OR ALTER FUNCTION Chat.FetchFollowing (
-	@userId INT,
+--'FetchFollowers' means fetch the users that follow @userId.
+CREATE OR ALTER PROCEDURE Chat.FetchFollowers
+	@userHandle NVARCHAR(30),
 	@page INT
-)
-RETURNS TABLE
 AS
-RETURN (
-	SELECT U.[name] AS userName,
-		U.handle AS userHandle,
-		I.imageUrl AS userImage,
-		U.bio AS userBio
-	FROM Chat.[User] U 
-		INNER JOIN Chat.Follower F ON @userId = F.followedUserId
-			AND U.userId = F.followerUserId
-		INNER JOIN Chat.[Image] I ON U.imageId = I.imageId
-	ORDER BY F.followDate DESC
-	OFFSET (@page * 20) ROWS FETCH NEXT 20 ROWS ONLY
-);
+DECLARE @userId INT = (SELECT U.userId FROM Chat.[User] U WHERE U.handle = @userHandle);
+SELECT U.[name] AS userName,
+	U.handle AS userHandle,
+	I.imageUrl AS userImage,
+	U.bio AS userBio
+FROM Chat.[User] U 
+	INNER JOIN Chat.Follower F ON @userId = F.followedUserId
+		AND U.userId = F.followerUserId
+	INNER JOIN Chat.[Image] I ON U.imageId = I.imageId
+ORDER BY F.followDate DESC
+OFFSET (@page * 20) ROWS FETCH NEXT 20 ROWS ONLY
 GO
 
 SELECT *
