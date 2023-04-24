193e8b6002d1434bc4c7d481cbaec0e460736b56
J-houghton <jakehoughton@ksu.edu>
2023-04-24 00:00:47 -0500

    moved completed todo into sql file, added aggregated Queries to Taskdone.sql

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -351,7 +351,7 @@ SET content = @content
 WHERE postId = @postId
 GO
 
-CREATE PROCEDURE Chat.LikePost
+CREATE OR ALTER PROCEDURE Chat.LikePost
     @postId INT,
     @userId INT,
 	@like INT
@@ -367,7 +367,7 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE Chat.FollowUser @followedUserId INT, @followerUserId INT, @follow INT
+CREATE OR ALTER PROCEDURE Chat.FollowUser @followedUserId INT, @followerUserId INT, @follow INT
 AS
 BEGIN
 	IF @follow = 1
@@ -386,6 +386,151 @@ IF EXISTS (SELECT * FROM Chat.[User] U WHERE U.handle = @handle)
 ELSE 
     SELECT 1 AS isValidHandle
 GO
+
+
+--Update User - INPUT: @userId, @image (IMAGE), @bio, @handle, @name, OUTPUT: @deletedImage (the publicId of the iamge that was replaced (if a new image was provided))
+CREATE OR ALTER PROCEDURE Chat.UpdateUser
+    @userId INT,
+    @imagePublicId NVARCHAR(100),
+    @bio NVARCHAR(150),
+    @handle NVARCHAR(30),
+    @name NVARCHAR(30),
+    @deletedImage NVARCHAR(100) OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @imageId INT;
+    DECLARE @publicId NVARCHAR(100);
+
+    -- Check if a new handle is provided and if it already exists
+    IF @handle IS NOT NULL
+    BEGIN
+        IF EXISTS (SELECT 1 FROM Chat.[User] WHERE handle = @handle AND userId != @userId)
+        BEGIN
+            RAISERROR('Handle already exists.', 16, 1);
+            RETURN;
+        END
+    END
+
+    -- Check if a new image was provided
+    IF @imagePublicId IS NOT NULL
+    BEGIN
+        -- Get imageId of the image to set
+        SELECT @imageId = imageId, @publicId = publicId
+        FROM Chat.[Image]
+        WHERE publicId = @imagePublicId;
+
+        -- Raise an error if the image publicId is not found
+        IF @imageId IS NULL
+        BEGIN
+            RAISERROR('Image publicId not found.', 16, 1);
+            RETURN;
+        END
+    END
+    ELSE
+    BEGIN
+        -- Get imageId and publicId of the image the user currently has
+        SELECT @imageId = imageId, @publicId = publicId
+        FROM Chat.[Image]
+        WHERE imageId = (SELECT imageId FROM Chat.[User] WHERE userId = @userId);
+    END
+
+    -- Update user information
+    UPDATE Chat.[User]
+    SET imageId = COALESCE(@imageId, imageId),
+        bio = COALESCE(@bio, bio),
+        handle = COALESCE(@handle, handle),
+        name = COALESCE(@name, name)
+    WHERE userId = @userId;
+
+    -- Update user image reference
+    IF @imageId IS NOT NULL
+    BEGIN
+        DECLARE @oldImageId INT;
+        SELECT @oldImageId = imageId FROM Chat.[User] WHERE userId = @userId;
+
+        IF @oldImageId IS NOT NULL
+        BEGIN
+            -- Check the number of users referencing the old image
+            DECLARE @userCount INT;
+            SELECT @userCount = COUNT(*) FROM Chat.[User] WHERE imageId = @oldImageId;
+
+            -- Delete old image if there are no other users referencing it
+            IF @userCount = 1
+            BEGIN
+                DELETE FROM Chat.[Image]
+                WHERE imageId = @oldImageId;
+            END
+        END
+
+        SET @deletedImage = @publicId;
+    END
+    ELSE
+    BEGIN
+        SET @deletedImage = NULL;
+    END
+END
+
+------Delete a User Post - INPUT: @postId, OUTPUT: none
+CREATE OR ALTER PROCEDURE Chat.DeleteUserPost
+    @postId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DELETE FROM Chat.Post
+    WHERE postId = @postId
+END
+---------------Fetch all users who liked a post - INPUT: @postId, @page, OUTPUT: userName ('User.name'), userImage ('Image.imageUrl'), userHandle ('User.handle'), bio ('User.bio')
+--NOTE: We will want to only return ~20 users for each stored procedure call. Use OFFSET-FETCH with the @page parameter to return the correct users
+CREATE OR ALTER PROCEDURE Chat.GetUsersWhoLikedPost
+    @postId INT,
+    @page INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @pageSize INT = 20
+    DECLARE @offset INT = (@page - 1) * @pageSize
+
+    SELECT userName, userImage, userHandle, bio
+    FROM (
+        SELECT [User].name AS userName,
+            Image.imageUrl AS userImage,
+            [User].handle AS userHandle,
+            [User].bio,
+            ROW_NUMBER() OVER (ORDER BY [Like].postId) AS rownum
+        FROM Chat.[Like]
+        INNER JOIN Chat.[User] ON [Like].userId = [User].userId
+        INNER JOIN Chat.[Image] ON [User].imageId = [Image].imageId
+        WHERE [Like].postId = @postId
+    ) AS subquery
+    WHERE rownum > @offset AND rownum <= (@offset + @pageSize)
+    ORDER BY rownum
+END
+
+--Fetch User Posts - INPUT: @userId, @page, OUTPUT: Same exact structure as the 'posts' array above except doesn't need to be JSON data (and we don't need to return 
+--the user info)
+--NOTE: Limit the amount of posts to 10 for each stored procedure call.
+CREATE OR ALTER PROCEDURE Chat.GetUserPosts
+    @userId INT,
+    @page INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @pageSize INT = 10
+    DECLARE @offset INT = (@page - 1) * @pageSize
+
+    SELECT Post.postId, Post.content, Post.replyToPostId, Post.isPinned, Post.createdOn
+    FROM Chat.Post
+    WHERE Post.userId = @userId
+    ORDER BY Post.createdOn DESC
+    OFFSET @offset ROWS
+    FETCH NEXT @pageSize ROWS ONLY
+END
+
 
 --Functions
 DROP FUNCTION IF EXISTS Chat.FetchImages;
--- TaskDone.sql
+++ TaskDone.sql
@@ -1,156 +1,108 @@
+--Aggregated Queries
+--1.
+CREATE VIEW Chat.ActiveUsersCount AS
+SELECT
+    COUNT(*) AS TotalActiveUsers
+FROM
+    Chat.[User]
+WHERE
+    status = 1;
 
---Update User - INPUT: @userId, @image (IMAGE), @bio, @handle, @name, OUTPUT: @deletedImage (the publicId of the iamge that was replaced (if a new image was provided))
---have not added to db file 
-CREATE PROCEDURE Chat.UpdateUser
-    @userId INT,
-    @imagePublicId NVARCHAR(100),
-    @bio NVARCHAR(150),
-    @handle NVARCHAR(30),
-    @name NVARCHAR(30),
-    @deletedImage NVARCHAR(100) OUTPUT
+--2.
+CREATE FUNCTION Chat.ActiveUsersInCommunity (@communityId INT)
+RETURNS TABLE
 AS
-BEGIN
-    SET NOCOUNT ON;
+RETURN (
+    SELECT
+        COUNT(*) AS TotalActiveUsersInSpecificCommunity
+    FROM
+        Chat.[User] u
+    JOIN
+        Chat.CommunityUser cu ON u.userId = cu.userId
+    WHERE
+        u.status = 1
+        AND cu.communityId = @communityId
+);
+--2.1 
+SELECT
+    TotalActiveUsersInSpecificCommunity
+FROM
+    Chat.ActiveUsersInCommunity(1); -- Replace 1 with the desired communityId
 
-    DECLARE @imageId INT;
-    DECLARE @publicId NVARCHAR(100);
-
-    -- Check if a new handle is provided and if it already exists
-    IF @handle IS NOT NULL
-    BEGIN
-        IF EXISTS (SELECT 1 FROM Chat.[User] WHERE handle = @handle AND userId != @userId)
-        BEGIN
-            RAISERROR('Handle already exists.', 16, 1);
-            RETURN;
-        END
-    END
-
-    -- Check if a new image was provided
-    IF @imagePublicId IS NOT NULL
-    BEGIN
-        -- Get imageId of the image to set
-        SELECT @imageId = imageId, @publicId = publicId
-        FROM Chat.[Image]
-        WHERE publicId = @imagePublicId;
-
-        -- Raise an error if the image publicId is not found
-        IF @imageId IS NULL
-        BEGIN
-            RAISERROR('Image publicId not found.', 16, 1);
-            RETURN;
-        END
-    END
-    ELSE
-    BEGIN
-        -- Get imageId and publicId of the image the user currently has
-        SELECT @imageId = imageId, @publicId = publicId
-        FROM Chat.[Image]
-        WHERE imageId = (SELECT imageId FROM Chat.[User] WHERE userId = @userId);
-    END
-
-    -- Update user information
-    UPDATE Chat.[User]
-    SET imageId = COALESCE(@imageId, imageId),
-        bio = COALESCE(@bio, bio),
-        handle = COALESCE(@handle, handle),
-        name = COALESCE(@name, name)
-    WHERE userId = @userId;
-
-    -- Update user image reference
-    IF @imageId IS NOT NULL
-    BEGIN
-        DECLARE @oldImageId INT;
-        SELECT @oldImageId = imageId FROM Chat.[User] WHERE userId = @userId;
-
-        IF @oldImageId IS NOT NULL
-        BEGIN
-            -- Check the number of users referencing the old image
-            DECLARE @userCount INT;
-            SELECT @userCount = COUNT(*) FROM Chat.[User] WHERE imageId = @oldImageId;
-
-            -- Delete old image if there are no other users referencing it
-            IF @userCount = 1
-            BEGIN
-                DELETE FROM Chat.[Image]
-                WHERE imageId = @oldImageId;
-            END
-        END
-
-        SET @deletedImage = @publicId;
-    END
-    ELSE
-    BEGIN
-        SET @deletedImage = NULL;
-    END
-END
-
-------Delete a User Post - INPUT: @postId, OUTPUT: none
-CREATE PROCEDURE Chat.DeleteUserPost
-    @postId INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    DELETE FROM Chat.Post
-    WHERE postId = @postId
-END
----------------Fetch all users who liked a post - INPUT: @postId, @page, OUTPUT: userName ('User.name'), userImage ('Image.imageUrl'), userHandle ('User.handle'), bio ('User.bio')
---NOTE: We will want to only return ~20 users for each stored procedure call. Use OFFSET-FETCH with the @page parameter to return the correct users
-CREATE PROCEDURE Chat.GetUsersWhoLikedPost
-    @postId INT,
-    @page INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    DECLARE @pageSize INT = 20
-    DECLARE @offset INT = (@page - 1) * @pageSize
-
-    SELECT userName, userImage, userHandle, bio
-    FROM (
-        SELECT [User].name AS userName,
-            Image.imageUrl AS userImage,
-            [User].handle AS userHandle,
-            [User].bio,
-            ROW_NUMBER() OVER (ORDER BY [Like].postId) AS rownum
-        FROM Chat.[Like]
-        INNER JOIN Chat.[User] ON [Like].userId = [User].userId
-        INNER JOIN Chat.[Image] ON [User].imageId = [Image].imageId
-        WHERE [Like].postId = @postId
-    ) AS subquery
-    WHERE rownum > @offset AND rownum <= (@offset + @pageSize)
-    ORDER BY rownum
-END
-
--------Create a post like - INPUT: @postId, @userId, OUTPUT: none
-CREATE PROCEDURE Chat.LikePost
-    @postId INT,
-    @userId INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    INSERT INTO Chat.[Like] (postId, userId)
-    VALUES (@postId, @userId)
-END
-
---Fetch User Posts - INPUT: @userId, @page, OUTPUT: Same exact structure as the 'posts' array above except doesn't need to be JSON data (and we don't need to return 
---the user info)
---NOTE: Limit the amount of posts to 10 for each stored procedure call.
-CREATE PROCEDURE Chat.GetUserPosts
-    @userId INT,
-    @page INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    DECLARE @pageSize INT = 10
-    DECLARE @offset INT = (@page - 1) * @pageSize
-
-    SELECT Post.postId, Post.content, Post.replyToPostId, Post.isPinned, Post.createdOn
+--3
+WITH PostsInLastMonth AS (
+    SELECT *
     FROM Chat.Post
-    WHERE Post.userId = @userId
-    ORDER BY Post.createdOn DESC
-    OFFSET @offset ROWS
-    FETCH NEXT @pageSize ROWS ONLY
-END
+    WHERE DATEADD(MONTH, -1, SYSDATETIMEOFFSET()) < createdOn
+),
+PostsInCommunities AS (
+    SELECT
+        postId,
+        communityId,
+        createdOn,
+        LAG(createdOn) OVER (PARTITION BY communityId ORDER BY createdOn) AS previousCreatedOn
+    FROM
+        PostsInLastMonth
+),
+TimeDifferenceInCommunities AS (
+    SELECT
+        communityId,
+        AVG(DATEDIFF(SECOND, previousCreatedOn, createdOn)) AS AvgTimeBtwnPosts
+    FROM
+        PostsInCommunities
+    WHERE
+        previousCreatedOn IS NOT NULL
+    GROUP BY
+        communityId
+)
+SELECT
+    c.communityId,
+    c.name,
+    tdic.AvgTimeBtwnPosts,
+    RANK() OVER (ORDER BY tdic.AvgTimeBtwnPosts ASC) AS Rank
+FROM
+    Chat.Community c
+JOIN
+    TimeDifferenceInCommunities tdic ON c.communityId = tdic.communityId
+ORDER BY
+    tdic.AvgTimeBtwnPosts;
+
+
+
+--4
+CREATE FUNCTION Chat.MostDiscussedPostsInCommunity (
+    @communityId INT,
+    @startDate DATETIMEOFFSET,
+    @endDate DATETIMEOFFSET
+)
+RETURNS TABLE
+AS
+RETURN (
+    SELECT
+        p.communityId,
+        p.postId,
+        COUNT(r.postId) AS replyToPost,
+        p.userId
+    FROM
+        Chat.Post p
+    LEFT JOIN
+        Chat.Post r ON p.postId = r.replyToPostId
+    WHERE
+        p.communityId = @communityId
+        AND p.createdOn BETWEEN @startDate AND @endDate
+    GROUP BY
+        p.communityId,
+        p.postId,
+        p.userId
+);
+
+--4.1
+SELECT
+    CommunityId,
+    PostId,
+    replyToPost,
+    userId
+FROM
+    Chat.MostDiscussedPostsInCommunity(1, '2023-01-01T00:00:00', '2023-01-31T23:59:59')
+ORDER BY
+    replyToPost DESC;
